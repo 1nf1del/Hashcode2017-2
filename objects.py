8d44c75779b67d3df87594327fd57b919e096d83
JujuYuki <me@jujuyuki.fr>
2017-02-23 19:30:01 +0100

    derp

--- objects.py
+++ objects.py
@@ -1,23 +1,23 @@
-Class endPoint:
+class EndPoint:
     def __init__(id,datacenterLatency):
         self.id = id
         self.datacenterLatency = datacenterLatency
         self.nbRequest = 0
         self.cacheServers = []
     
-Class cacheServer:
+class CacheServer:
     def __init__(capacity):
         self.capacity = capacity
         self.content = []
         self.endPoints = []
     
-Class request:
+class Request:
     def __init__(vidId,endPoint,nbReq):
         self.vidId = vidId
         self.endPoint = endPoint
         self.nbReq = nbReq
         
-Class video:
+class Video:
     def __init__(id,size):
         self.id = id
         self.size = size
