ed5a02b505774145c8710dae24c392a04a634aa5
JujuYuki <me@jujuyuki.fr>
2017-02-23 21:25:18 +0100

    output parser

---
+++ parser_out.py
@@ -0,0 +1,17 @@
+from objects import *
+path="kittens.out"
+
+def parsing_out(CacheServList,path):
+    data=open(path,'w')
+    N = 0
+    for k,cache in CacheServList.items():
+        if cache.freeSpace < cache.capacity:
+            N += 1
+    data.write("%i" % N)
+    for k,cache in CacheServList.items():
+        if cache.freeSpace < cache.capacity:
+            data.write("\n%i" % cache.csId)
+            for i in cache.content:
+                data.write(" %i" % i)
+    data.write("\n")
+    data.close()
