7d85a29ef40f164b422b6f86441a8077f627e8ee
JujuYuki <me@jujuyuki.fr>
2017-02-23 19:55:08 +0100

    objects update

--- objects.py
+++ objects.py
@@ -1,23 +1,40 @@
 class EndPoint:
-    def __init__(id,datacenterLatency):
-        self.id = id
+    def __init__(self,epId,cacheServers,datacenterLatency):
+        self.epId = epId
         self.datacenterLatency = datacenterLatency
         self.nbRequest = 0
-        self.cacheServers = []
+        self.cacheServers = cacheServers
+        
+    def addRequest(self,nbReq):
+        self.nbRequest += nbReq
+
+    def assignRequestsToEndPoint(self,reqList):
+        for req in reqList:
+            if req.endPoint == self.id:
+                self.addRequest(req.nbRequest)
     
 class CacheServer:
-    def __init__(capacity):
+    def __init__(self,csId,capacity):
         self.capacity = capacity
+        self.csId = csId
         self.content = []
         self.endPoints = []
+        self.isUsed = False
+        
+    def equality(self,csId):
+        return csId == self.csId
+    
+    def isUsed(self):
+        return self.isUsed
     
 class Request:
-    def __init__(vidId,endPoint,nbReq):
+    def __init__(self,vidId,endPoint,nbReq):
         self.vidId = vidId
         self.endPoint = endPoint
-        self.nbReq = nbReq
+        self.nbRequest = nbReq
         
 class Video:
-    def __init__(id,size):
-        self.id = id
+    def __init__(self,vidId,size):
+        self.vidId = vidId
         self.size = size
+        
